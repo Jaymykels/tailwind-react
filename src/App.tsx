4f22de0a2ec7ed546338a3230066f554ede83da7
Chukwuemeka.Ezeokoye <chukwuemeka.ezeokoye@interswitchgroup.com>
2019-08-19 07:51:49 +0100

    installed tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="m-auto antialiased font-sans font-serif font-mono text-center">
+      <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-2xl">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-blue-300"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Taiwlind with React TypeScript
         </a>
       </header>
     </div>
